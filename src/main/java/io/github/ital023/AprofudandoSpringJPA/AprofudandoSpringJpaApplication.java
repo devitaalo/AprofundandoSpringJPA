27b398e3e9e49b6615c9eb6db9130432dc355a6f
Ital02 <devitalomiranda@gmail.com>
2024-07-02 18:22:58 -0300

    feat: first commit

---
+++ src/main/java/io/github/ital023/AprofudandoSpringJPA/AprofudandoSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package io.github.ital023.AprofudandoSpringJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprofudandoSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprofudandoSpringJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ital023/AprofudandoSpringJPA/AprofudandoSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ital023.AprofudandoSpringJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprofudandoSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
